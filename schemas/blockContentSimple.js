e2a95858d6846e9e133652a0c9e7565a245c16ce
mattwelson <matthew.welson@gmail.com>
2020-05-20 21:03:18 +1200

    'init

---
+++ schemas/blockContentSimple.js
@@ -0,0 +1,26 @@
+export default {
+  title: "Block Content Simple",
+  name: "blockContentSimple",
+  type: "array",
+  of: [
+    {
+      title: "Block",
+      type: "block",
+      // Styles let you set what your user can mark up blocks with. These
+      // correspond with HTML tags, but you can set any title or value
+      // you want and decide how you want to deal with it where you want to
+      // use your content.
+      styles: [{ title: "Normal", value: "normal" }],
+      lists: [],
+      // Marks let you mark up inline text in the block editor.
+      marks: {
+        // Decorators usually describe a single property – e.g. a typographic
+        // preference or highlighting by editors.
+        decorators: [
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
+        ],
+      },
+    },
+  ],
+}
---
+++ schemas/me.js
@@ -0,0 +1,76 @@
+export default {
+  name: "me",
+  title: "Me",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "shortName",
+      title: "Short name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "shortBio",
+      title: "Short bio",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
+        },
+      ],
+    },
+    {
+      name: "bio",
+      title: "Full bio",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
+        },
+      ],
+    },
+    {
+      title: "Twitter",
+      name: "twitter",
+      type: "string",
+    },
+    {
+      title: "Instagram",
+      name: "instagram",
+      type: "string",
+    },
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+    },
+  },
+}
